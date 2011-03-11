8b5456dd5422ba6a13e704aa513c8207bb6c649b
Timur Vafin <timur.vafin@flatsoft.com>
2011-03-11 18:06:30 +0300

    Added ruby env for GC based on 37signals settings

--- common/misc-env.sh
+++ common/misc-env.sh
@@ -1,3 +1,12 @@
+# http://www.coffeepowered.net/2009/06/13/fine-tuning-your-garbage-collector/
+# http://snaprails.tumblr.com/post/241746095/rubys-gc-configuration
+# 37signals’s settings
+export RUBY_HEAP_MIN_SLOTS=600000
+export RUBY_HEAP_SLOTS_INCREMENT=10000
+export RUBY_HEAP_SLOTS_GROWTH_FACTOR=1.8
+export RUBY_GC_MALLOC_LIMIT=59000000
+export RUBY_HEAP_FREE_MIN=100000
+
 export GREP_OPTIONS='--color=auto'
 export LS_OPTIONS=' -G'
 
