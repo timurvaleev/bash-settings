4d35b1701a325e7a6e1477b551b81a5b9cc6ee88
Timur Vafin <timur.vafin@flatsoft.com>
2011-03-28 19:06:49 +0400

    Added aliases for bundle

--- local.sh
+++ local.sh
@@ -16,6 +16,7 @@ export PS1='\[\033[0;37m\]\w\[\033[00m\] \$ ' # override PS1 to a simpler format
 . $dir/desktop/editors.sh
 . $dir/desktop/flash-log.sh
 . $dir/desktop/work.sh
+. $dir/desktop/bundle-shortcuts.sh
 
 . $dir/common/git.sh
 . $dir/common/git-one-letter-aliases.sh
